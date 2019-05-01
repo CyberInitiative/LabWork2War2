643ccd4acdd10cb97e743eee3bbd90c7ac06d8c9
Мирослав Левдиков <deadstronghold@gmail.com>
2019-05-01 19:37:34 +0300

    Разбив программы на классы

---
+++ ConsoleApp7/IsoscelesTriangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class IsoscelesTriangle : Triangle
+    {
+        public IsoscelesTriangle(double sideBase, double sideCat) : base(sideBase, sideCat, sideCat)
+        {
+        }
+
+        public bool IsIsoscelesTriangle()
+        {
+            return this.sideB == this.sideC;
+        }
+    }
+}
